b5b9d8255e624425a87abb48a6185a58d82eea86
NEU-Liu <1531653614@qq.com>
2020-04-05 17:48:03 +0800

    first commit

---
+++ CMakeFiles/MyH264BSD.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/liu/CLionProjects/MyH264BSD/main.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/main.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_byte_stream.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_byte_stream.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_cavlc.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_cavlc.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_conceal.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_conceal.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_deblocking.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_deblocking.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_decoder.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_decoder.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_dpb.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_dpb.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_image.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_image.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_inter_prediction.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_inter_prediction.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_intra_prediction.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_intra_prediction.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_macroblock_layer.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_macroblock_layer.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_nal_unit.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_nal_unit.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_neighbour.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_neighbour.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_pic_order_cnt.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_pic_order_cnt.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_pic_param_set.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_pic_param_set.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_reconstruct.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_reconstruct.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_sei.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_sei.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_seq_param_set.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_seq_param_set.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_slice_data.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_slice_data.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_slice_group_map.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_slice_group_map.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_slice_header.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_slice_header.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_storage.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_storage.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_stream.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_stream.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_transform.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_transform.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_util.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_util.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_vlc.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_vlc.c.o"
+  "/home/liu/CLionProjects/MyH264BSD/src/h264bsd_vui.c" "/home/liu/CLionProjects/MyH264BSD/CMakeFiles/MyH264BSD.dir/src/h264bsd_vui.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/MyH264BSD.dir/cmake_clean.cmake
@@ -0,0 +1,36 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyH264BSD.dir/main.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_byte_stream.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_cavlc.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_conceal.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_deblocking.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_decoder.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_dpb.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_image.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_inter_prediction.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_intra_prediction.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_macroblock_layer.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_nal_unit.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_neighbour.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_pic_order_cnt.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_pic_param_set.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_reconstruct.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_sei.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_seq_param_set.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_slice_data.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_slice_group_map.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_slice_header.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_storage.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_stream.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_transform.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_util.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_vlc.c.o"
+  "CMakeFiles/MyH264BSD.dir/src/h264bsd_vui.c.o"
+  "MyH264BSD"
+  "MyH264BSD.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MyH264BSD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
